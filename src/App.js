54a077c85b0df34f620bc56e551d96122b56070f
Nisha Kumari <89996949+t1kuni01@users.noreply.github.com>
2022-09-27 12:38:39 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+
+const URL = 'https://api.exchangerate.host/latest';
+const API_KEY = '911b3806a1cbe40dacf96b52c007b3f3';
+
+function App() {
+const  [eur, setEur] = useState(0);
+const  [gbp, setGbp] = useState(0);
+const  [rate, setRate] = useState(0);
+
+async function convert(e) {
+  e.preventDefault();
+  try {
+    const address = URL + API_KEY;
+    const response = await fetch(address);
+
+    if (response.ok) {
+      const json = await response.json();
+      console.log(json.rates.GBP);
+      setRate(json.rates.GBP);
+      setGbp(eur * json.rates.GBP);
+    } else {
+      alert('Error retrieving exchange rate.');
+      console.log(response);
+    }
+  } catch (err) {
+    alert(err);
+  }
+}
+
+
+  return (
+    <div id="container">
+      <form onSubmit={convert}>
+      <div>
+          <label>Eur</label>&nbsp;
+          <input type="number" step="0.01"
+          value={eur} onChange={e => setEur(e.target.value)} />
+          <output>{rate}</output>
+        </div>
+
+        <div>
+          <label>Gbp</label>
+          <output>{gbp.toFixed(2)} €</output>
+        </div>
+
+        <div>
+          <button>Calculate</button>
+        </div>
+        </form>
+        
+    </div>
+  );
+
+  
+}
+
+
+export default App;
+
